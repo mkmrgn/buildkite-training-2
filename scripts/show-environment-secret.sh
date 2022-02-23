146588c50d415d465b2112b91df92668bd3cedf5
mkmrgn <mike@morgan.ms>
2022-02-22 19:23:48 -0800

    added block steps

--- scripts/show-environment-secret.sh
+++ scripts/show-environment-secret.sh
@@ -1,4 +1,3 @@
-echo $SUDO_USER
 echo agent environment secret: $MY_ENVIRONMENT_SECRET
 if [[ "$MY_ENVIRONMENT_SECRET" == "correct horse battery staple" ]]; then
   echo "^^ password is correct, though"
