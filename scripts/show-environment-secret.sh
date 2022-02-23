b9b7535bb073e6c850fc09b9c8efa8ba180e5313
mkmrgn <mike@morgan.ms>
2022-02-22 19:03:11 -0800

    sdfsdfs

--- scripts/show-environment-secret.sh
+++ scripts/show-environment-secret.sh
@@ -1,3 +1,4 @@
+echo $SUDO_USER
 echo agent environment secret: $MY_ENVIRONMENT_SECRET
 if [[ "$MY_ENVIRONMENT_SECRET" == "correct horse battery staple" ]]; then
   echo "^^ password is correct, though"
