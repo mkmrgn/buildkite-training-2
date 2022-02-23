184ed64b6d9c38e5ac23e2f056fbf443de201bb6
mkmrgn <mike@morgan.ms>
2022-02-22 17:52:55 -0800

    sdfsd

--- scripts/show-environment-secret.sh
+++ scripts/show-environment-secret.sh
@@ -1,5 +1,5 @@
 echo $MY_ENVIRONMENT_SECRET;
 if [[ "$MY_ENVIRONMENT_SECRET" == "correct horse battery staple" ]]; then
-  echo -e "<--- password is correct, though\n"
+  echo "<--- password is correct, though"
 fi
-echo environment thing: $MY_ENVIRONMENT_THING
+echo $"environment thing: $MY_ENVIRONMENT_THING"
